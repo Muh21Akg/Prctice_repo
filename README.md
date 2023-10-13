7eb36e9ddd72acaf640154ea80a490c1840e31c8
muh21akg <muhakg@yahoo.com>
2023-10-13 06:45:00 -0700

    header

--- README.md
+++
@@ -1,1 +0,0 @@
-hello fam here is our practice repo, you can make your first commit by uploading a file or editting this readme file
---
+++ printf.h
@@ -0,0 +1,23 @@
+#ifndef PRINTF
+#define PRINTF
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _pchar(valist c);
+int _pstring(valist s);
+
+/**
+ * struct format - structure of type format with two elements
+ * @src: placeholder
+ * @dis: function to process it
+ */
+typedefvstruct format
+{
+	char *src;
+	int (*dis)(va_list);
+} form;
+
+#endif
